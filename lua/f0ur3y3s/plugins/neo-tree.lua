5690f09a9963395c38c37a387d908e7229925cdc
Daniel Chung <daniel.y.chung.mil@army.mil>
2024-06-27 17:07:05 +0000

    Fixing neotree

--- lua/f0ur3y3s/plugins/neo-tree.lua
+++ lua/f0ur3y3s/plugins/neo-tree.lua
@@ -9,9 +9,5 @@ return {
     config = function()
         vim.keymap.set("n", "<C-n>", "<Cmd>Neotree toggle<CR>", { noremap = true, silent = true})
         vim.keymap.set("n", "<leader>bf", "<Cmd>Neotree source=buffers position=float toggle<CR>", {noremap = true, silent = true})
-        -- local kmp = vim.keymap
-        -- kmp.set("n", "<C-n>", "<Cmd>lua require'nvim-tree'.toggle()<CR>")
-        -- vim.keymap.set("n", "<C-n>", ":Neotree source=filesystem reveal=true position=left toggle=true<CR>", {})
-        -- vim.keymap.set("n", "<leader>bf", ":Neotree source=buffers reveal position=float<CR>", {})
     end,
 }
