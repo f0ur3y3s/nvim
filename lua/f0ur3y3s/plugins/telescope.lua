ef8b6da8af15505e96b68b594ee3d0358f078220
f0ur3y3s <dchung0812@gmail.com>
2025-01-25 12:22:23 -0500

    fixing keybinds

--- lua/f0ur3y3s/plugins/telescope.lua
+++ lua/f0ur3y3s/plugins/telescope.lua
@@ -30,7 +30,8 @@ return {
             --        kmp.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
             kmp.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
             kmp.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
-            kmp.set("n", "<leader>bf", builtin.buffers, { desc = "Telescope buffers" })
+            kmp.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+            kmp.set("n", "<leader>/", builtin.current_buffer_fuzzy_find, { desc = "Telescope current buffer fuzzy find" })
 
             require("telescope").load_extension("ui-select")
         end,
