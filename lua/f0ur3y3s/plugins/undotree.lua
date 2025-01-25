10155f5a3a68855e3447d6474be45ec72763f1f4
f0ur3y3s <dchung0812@gmail.com>
2025-01-25 17:38:23 -0500

    unsure about undotree

---
+++ lua/f0ur3y3s/plugins/undotree.lua
@@ -0,0 +1,9 @@
+return {
+    -- "mbbill/undotree",
+    -- config = function()
+    --     vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "Open [U]ndotree" })
+    --     vim.g.undotree_WindowLayout = 1
+    --     vim.undotree_ShortIndicators = true
+    --     vim.undotree_ShortIndicators = true
+    -- end,
+}
