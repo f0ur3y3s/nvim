25c5a36a5a61cc6dd9cac966607ad1516c45435c
f0ur3y3s <dchung0812@gmail.com>
2025-09-02 20:02:03 -0400

    update

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,15 +1,31 @@
-return { 
-    'nvim-mini/mini.nvim', 
+return {
+    'nvim-mini/mini.nvim',
     version = "*",
     config = function()
         require("mini.pairs").setup()
         require("mini.ai").setup()
+        require("mini.files").setup()
 
         -- appearance
         require("mini.icons").setup()
         require("mini.statusline").setup()
         require("mini.notify").setup()
-        require("mini.trailspace").setup()
+        require("mini.trailspace").setup({
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                pattern = "*",
+                callback = function()
+                    MiniTrailspace.trim()
+                end,
+            }),
+
+            vim.api.nvim_create_autocmd("ModeChanged", {
+                pattern = "*:n",  -- Any mode to normal mode
+                callback = function()
+                    MiniTrailspace.trim()
+                end,
+            })
+        })
         require("mini.tabline").setup()
+        require("mini.cursorword").setup()
     end,
 }
--- lua/plugins/which_key.lua
+++ lua/plugins/which_key.lua
@@ -8,14 +8,30 @@ return {
             preset = "helix",
         })
 
-        -- {{{ BASICS
+        local minifiles_toggle = function(...)
+            if not MiniFiles.close() then MiniFiles.open(...) end
+        end
+
+        --- {{{ BASICS
         wk.add({
             { "<A-j>", ":m .+1<CR>==", desc = "Move line down" },
             { "<A-k>", ":m .-2<CR>==", desc = "Move line up" },
             { "<A-j>", ":m '>+1<CR>gv=gv", desc = "Move selection down", mode = "v" },
             { "<A-k>", ":m '<-2<CR>gv=gv", desc = "Move selection up", mode = "v" },
+            { "<C-u>", "<C-u>zz", desc = "Page Up (centered)" },
+            { "<C-d>", "<C-d>zz", desc = "Page Down (centered)" },
         })
-        -- }}}
+        --- }}}
+
+        --- MINI CONFIG
+        wk.add({
+            { "<leader>e", minifiles_toggle, desc = "Opens file explorer" },
+            { "<leader>tw", "<cmd>lua MiniTrailspace.trim()<cr>", desc = "Trim trailing whitespace" },
+        })
+
+        wk.add({
+            { "<leader>L", "<cmd>Lazy<cr>", desc = "Open Lazy"}
+        })
 
     end,
 }
